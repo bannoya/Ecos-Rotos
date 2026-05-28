2e3c628bb410b93ad4bdec2c8a1af55567dd3475
Juan <jsebach99@gmail.com>
2026-05-28 17:27:27 -0300

    Ajustando mecanica de salto y empezando a crear parte tutorial

--- Assets/Proyect/Scripts/Player/PlayerMovement.cs
+++ Assets/Proyect/Scripts/Player/PlayerMovement.cs
@@ -4,8 +4,15 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
     public float jumpForce = 7f;
-    public float groundCheckDistance = 1.1f;
+
+    [Header("Ground Check")]
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
     public LayerMask groundLayer;
+
+    [Header("Salto")]
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
 
     private Rigidbody rb;
     private PlayerControls controls;
@@ -36,16 +43,20 @@ public class PlayerMovement : MonoBehaviour
     {
         moveInput = controls.Player.Move.ReadValue<Vector2>();
 
-        isGrounded = Physics.Raycast(
-            transform.position,
-            Vector3.down,
-            groundCheckDistance,
+        isGrounded = Physics.CheckSphere(
+            groundCheck.position,
+            groundCheckRadius,
             groundLayer
         );
 
-        if (controls.Player.Jump.triggered && isGrounded)
+        if (controls.Player.Jump.WasPressedThisFrame() && isGrounded)
         {
-            rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+            rb.linearVelocity = new Vector3(
+                rb.linearVelocity.x,
+                0f,
+                rb.linearVelocity.z
+            );
+
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
     }
@@ -57,5 +68,28 @@ public class PlayerMovement : MonoBehaviour
             rb.linearVelocity.y,
             0f
         );
+
+        AplicarGravedadMejorada();
+    }
+
+    void AplicarGravedadMejorada()
+    {
+        if (rb.linearVelocity.y < 0)
+        {
+            rb.AddForce(Vector3.down * fallMultiplier, ForceMode.Acceleration);
+        }
+
+        if (rb.linearVelocity.y > 0 && !controls.Player.Jump.IsPressed())
+        {
+            rb.AddForce(Vector3.down * lowJumpMultiplier, ForceMode.Acceleration);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (groundCheck != null)
+        {
+            Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+        }
     }
 }
